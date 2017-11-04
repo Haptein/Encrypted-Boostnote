ca8ab69cd0b9915eb0bb65bee0b9799c7ae5cc8f
Haptein <bruno.alatriste@gmail.com>
2017-11-04 17:11:30 -0600

    Folder encryption with AES

---
+++ ebnote.sh
@@ -0,0 +1,29 @@
+user=""
+backup="/home/$user/Documents/.Boostnote.tar.gz.gpg.backup"
+encrypted="/home/$user/Documents/.Boostnote.tar.gz.gpg"
+decrypted="Boostnote.tar.gz"
+
+#Prev Cleanup
+if [ -d /tmp/Boostnote ]; then
+    rm -r /tmp/Boostnote
+fi
+
+#Decrypt & Decompress
+cd /tmp/
+pass=`zenity --entry --text="Enter your passphrase" --hide-text --title="Encrypted Boostnote"`
+gpg --lock-multiple --batch --passphrase $pass -d $encrypted | tar xzf - Boostnote
+
+#if succesfully decrypted
+if [ -d /tmp/Boostnote ]; then
+    #Backup
+    cp -u $encrypted $backup
+
+    #Run
+    boostnote
+
+    #Compress & Encrypt
+    tar czf - Boostnote/ | gpg --batch --passphrase $pass -o $encrypted --symmetric --force-mdc --yes
+    rm -r Boostnote/
+else
+    echo "Wrong Passphrase!"
+fi
